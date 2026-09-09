0bb4001aa4ed8c2080bf3159b469269d9a02956f
sangram72 <sangramnandi72@gmail.com>
2026-09-09 11:46:21 +0530

    chnaged the apiservice

--- src/Features/Chat/Services/ChatApiservice.ts
+++ src/Features/Chat/Services/ChatApiservice.ts
@@ -1,14 +1,16 @@
 import { GoogleGenAI } from '@google/genai';
 
 const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE_API_KEY
+  apiKey: import.meta.env.VITE_API_KEY,
 });
 
-export const GET_CHAT = async (prompt: string) => {
+export const GET_CHAT = async (
+  prompt: string
+): Promise<string> => {
   const response = await ai.models.generateContent({
     model: 'gemini-3.8-flash',
     contents: prompt,
   });
 
-  return response.text;
+  return response.text ?? '';
 };
