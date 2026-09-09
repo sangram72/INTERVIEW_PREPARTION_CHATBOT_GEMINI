a2958785ed5a29b14282d979259d3709e2dde32c
sangram72 <sangramnandi72@gmail.com>
2026-09-09 11:34:57 +0530

    updated api config

--- src/Features/Chat/Services/ChatApiservice.ts
+++ src/Features/Chat/Services/ChatApiservice.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from '@google/genai';
 
 const ai = new GoogleGenAI({
-  apiKey: import.meta.env.API_KEY
+  apiKey: process.env.API_KEY 
 });
 
 export const GET_CHAT = async (prompt: string) => {
