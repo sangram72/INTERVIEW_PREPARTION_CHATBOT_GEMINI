a49ed2379d0f9258987922ab662fb4d3d86955c6
sangram72 <sangramnandi72@gmail.com>
2026-09-09 11:51:39 +0530

    model changed

--- src/Features/Chat/Services/ChatApiservice.ts
+++ src/Features/Chat/Services/ChatApiservice.ts
@@ -14,7 +14,7 @@ export const GET_CHAT = async (
   });
 
   const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
+    model: 'gemini-3.6-flash',
     contents: prompt,
   });
 
